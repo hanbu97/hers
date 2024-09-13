dc8645a92255892c4e3d1e5ac21672ac654fcab4
han <hanbu97@proton.me>
2024-09-13 18:27:02 +0800

    feat: uniform sampler

--- src/math/sampler/uniform_sampler.rs
+++ src/math/sampler/uniform_sampler.rs
@@ -1,117 +1,62 @@
-use std::cell::Cell;
+use rand_core::RngCore;
 
-use crate::math::ring::{polynomial::Poly, reduction::conditional::c_red, Ring};
+use crate::math::ring::{polynomial::Poly, Ring};
 
-use super::{traits::Sampler, KeyedPRNG};
-
-/// Uniform sampler for polynomials.
-pub struct UniformSampler {
+pub struct UniformSampler<R: RngCore + Clone> {
     base_ring: Ring,
-    prng: KeyedPRNG,
-    random_buffer: Vec<u8>,
-    ptr: Cell<usize>,
+    prng: R,
 }
 
-impl UniformSampler {
-    /// Creates a new UniformSampler.
-    ///
-    /// # Arguments
-    ///
-    /// * `prng` - A keyed pseudo-random number generator.
-    /// * `base_ring` - The ring in which polynomials will be sampled.
-    pub fn new(prng: KeyedPRNG, base_ring: Ring) -> Self {
-        let n = base_ring.degree() as usize;
+impl<R: RngCore + Clone> UniformSampler<R> {
+    pub fn new(prng: R, base_ring: Ring) -> Self {
+        UniformSampler { base_ring, prng }
+    }
+
+    pub fn at_level(&self, level: usize) -> Self {
         UniformSampler {
-            base_ring,
-            prng,
-            random_buffer: vec![0; n.max(1024)],
-            ptr: Cell::new(0),
+            base_ring: self.base_ring.at_level(level),
+            prng: self.prng.clone(),
         }
     }
 
-    /// Internal method for reading or adding sampled values.
-    ///
-    /// # Arguments
-    ///
-    /// * `pol` - The polynomial to be filled or added to.
-    /// * `f` - A function that determines how to combine existing and new values.
-    fn read_internal<F>(&mut self, pol: &mut Poly, f: F)
+    pub fn read(&mut self, pol: &mut Poly) {
+        self.read_inner(pol, |_, b, _| b);
+    }
+
+    pub fn read_and_add(&mut self, pol: &mut Poly) {
+        self.read_inner(pol, |a, b, c| (a + b) % c);
+    }
+
+    fn read_inner<F>(&mut self, pol: &mut Poly, f: F)
     where
         F: Fn(u64, u64, u64) -> u64,
     {
-        let level = self.base_ring.level;
-        let n = self.base_ring.degree() as usize;
-
-        let mut ptr = self.ptr.get();
+        let level = self.base_ring.moduli_chain().len() - 1;
+        let n = self.base_ring.degree();
 
         for j in 0..=level {
-            let qi = self.base_ring.sub_rings[j].modulus;
-            let mask = self.base_ring.sub_rings[j].mask;
-
-            let coeffs = &mut pol.coeffs[j];
+            let qi = self.base_ring.moduli_chain()[j];
+            let mask = (1u64 << (qi.ilog2() + 1)) - 1;
 
             for i in 0..n {
-                let random_uint = loop {
-                    if ptr == 0 || ptr == n {
-                        self.prng.read(&mut self.random_buffer).unwrap();
-                        ptr = 0;
-                    }
+                let mut random_uint = self.prng.next_u64() & mask;
+                while random_uint >= qi {
+                    random_uint = self.prng.next_u64() & mask;
+                }
 
-                    let random_uint =
-                        u64::from_be_bytes(self.random_buffer[ptr..ptr + 8].try_into().unwrap())
-                            & mask;
-                    ptr += 8;
-
-                    if random_uint < qi {
-                        break random_uint;
-                    }
-                };
-
-                coeffs[i] = f(coeffs[i], random_uint, qi);
+                pol.coeffs[j][i as usize] = f(pol.coeffs[j][i as usize], random_uint, qi);
             }
         }
-
-        self.ptr.set(ptr);
     }
 
-    /// Creates a new UniformSampler with a different PRNG.
-    pub fn with_prng(&self, prng: KeyedPRNG) -> Self {
-        UniformSampler {
-            base_ring: self.base_ring.clone(),
-            prng,
-            random_buffer: self.random_buffer.clone(),
-            ptr: Cell::new(0),
-        }
-    }
-}
-
-impl Sampler for UniformSampler {
-    fn read(&mut self, pol: &mut Poly) {
-        self.read_internal(pol, |_, b, _| b);
-    }
-
-    fn read_new(&mut self) -> Poly {
+    pub fn read_new(&mut self) -> Poly {
         let mut pol = self.base_ring.new_poly();
         self.read(&mut pol);
         pol
     }
-
-    fn read_and_add(&mut self, pol: &mut Poly) {
-        self.read_internal(pol, |a, b, c| c_red(a.wrapping_add(b), c));
-    }
-
-    fn at_level(&self, level: usize) -> Self {
-        UniformSampler {
-            base_ring: self.base_ring.at_level(level),
-            prng: KeyedPRNG::new(&self.prng.key()),
-            random_buffer: self.random_buffer.clone(),
-            ptr: Cell::new(0),
-        }
-    }
 }
 
-/// Samples a uniform random integer in the range [0, v-1].
-pub fn rand_uniform(prng: &mut KeyedPRNG, v: u64, mask: u64) -> u64 {
+pub fn rand_uniform<R: RngCore>(prng: &mut R, v: u64, mask: u64) -> u64 {
     loop {
         let random_int = rand_int64(prng, mask);
         if random_int < v {
@@ -120,59 +65,89 @@ pub fn rand_uniform(prng: &mut KeyedPRNG, v: u64, mask: u64) -> u64 {
     }
 }
 
-/// Samples a uniform variable in the range [0, mask], where mask is of the form 2^n-1, with n in [0, 32].
-pub fn rand_int32(prng: &mut KeyedPRNG, mask: u64) -> u64 {
-    let mut random_bytes = [0u8; 4];
-    prng.read(&mut random_bytes)
-        .expect("Failed to read from PRNG");
-    let random_uint32 = u32::from_be_bytes(random_bytes) as u64;
-    mask & random_uint32
-}
-
-/// Samples a uniform variable in the range [0, mask], where mask is of the form 2^n-1, with n in [0, 64].
-pub fn rand_int64(prng: &mut KeyedPRNG, mask: u64) -> u64 {
+fn rand_int64<R: RngCore>(prng: &mut R, mask: u64) -> u64 {
     let mut random_bytes = [0u8; 8];
-    prng.read(&mut random_bytes)
-        .expect("Failed to read from PRNG");
-    let random_uint64 = u64::from_be_bytes(random_bytes);
-    mask & random_uint64
+    prng.fill_bytes(&mut random_bytes);
+    u64::from_be_bytes(random_bytes) & mask
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::utils::rand::fixed_rand::FixedRandom;
+    use rand::SeedableRng;
+    use rand_chacha::ChaCha20Rng;
 
     #[test]
-    fn test_rand_uniform() {
-        let mut prng = KeyedPRNG::new(&[0u8; 32]);
-        let v = 100;
-        let mask = 0x7f; // 2^7 - 1
+    fn test_uniform_sampler() {
+        let ring = Ring::new(16, vec![97]).unwrap();
+        let prng = ChaCha20Rng::seed_from_u64(12345);
+        let mut sampler = UniformSampler::new(prng, ring);
 
-        for _ in 0..1000 {
-            let random_int = rand_uniform(&mut prng, v, mask);
-            assert!(random_int < v);
+        let pol = sampler.read_new();
+
+        // Basic sanity checks
+        assert_eq!(pol.coeffs.len(), 1);
+        assert_eq!(pol.coeffs[0].len(), 16);
+
+        // Check if coefficients are within bounds
+        for coeff in &pol.coeffs[0] {
+            assert!(*coeff < 97);
         }
     }
 
     #[test]
-    fn test_rand_int32() {
-        let mut prng = KeyedPRNG::new(&[0u8; 32]);
-        let mask = 0xffffffff; // 2^32 - 1
+    fn test_uniform_sampler_for_go_comparison() {
+        let degree = 16;
+        let modulus = 97u64;
 
-        for _ in 0..1000 {
-            let random_int = rand_int32(&mut prng, mask);
-            assert!(random_int <= mask);
-        }
-    }
+        let ring = Ring::new(degree, vec![modulus]).unwrap();
 
-    #[test]
-    fn test_rand_int64() {
-        let mut prng = KeyedPRNG::new(&[0u8; 32]);
-        let mask = 0xffffffffffffffff; // 2^64 - 1
+        let random_numbers: Vec<u64> = vec![
+            12300691266768169957,
+            11820962631477129540,
+            17883164048736053457,
+            3763972486863574293,
+            1820076341489734165,
+            3641032664938027597,
+            13856849471837345700,
+            11093809191189476932,
+            3825727900517507516,
+            1833057533073052521,
+            1862332976669812221,
+            839969800260750646,
+            5102443919682075139,
+            3469586806049992227,
+            1289589544690565689,
+            4686543058686886181,
+            4863912002469269231,
+            5872498502673663872,
+            10304755672215287095,
+            1191274978463715656,
+        ];
+        let fixed_rng = FixedRandom::new(random_numbers.clone());
 
-        for _ in 0..1000 {
-            let random_int = rand_int64(&mut prng, mask);
-            assert!(random_int <= mask);
+        let mut sampler = UniformSampler::new(fixed_rng, ring.clone());
+
+        let mut pol = ring.new_poly();
+
+        sampler.read(&mut pol);
+
+        let coeffs = &pol.coeffs[0];
+
+        let expected_coeffs = vec![68, 81, 21, 21, 77, 36, 68, 60, 54, 3, 35, 57, 37, 0, 55, 72];
+        assert_eq!(
+            coeffs, &expected_coeffs,
+            "Sampled coefficients do not match Go implementation"
+        );
+
+        for (i, &coeff) in coeffs.iter().enumerate() {
+            assert!(
+                coeff < modulus,
+                "Coefficient at index {} is out of range: {}",
+                i,
+                coeff
+            );
         }
     }
 }
